154bd39858d70d0da42c87ad9d07aa06df238061
SirCoder12 <136838859+SirCoder12@users.noreply.github.com>
2025-01-22 13:51:09 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ class _UiSettings(BaseSettings):
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Karasutoshomyo"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_description: str = "Kuko henko karasu to shomyo Hyori no Hazama"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True

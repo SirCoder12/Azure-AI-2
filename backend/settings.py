bcf3535d91be1e6aa37afa3628a5782268d371a7
SirCoder12 <136838859+SirCoder12@users.noreply.github.com>
2025-01-22 13:49:54 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,10 +41,10 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Gaki"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
+    chat_title: str = "Karasutoshomyo"
     chat_description: str = "This chatbot is configured to answer your questions"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
